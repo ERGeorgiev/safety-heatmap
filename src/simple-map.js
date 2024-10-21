19337b1ee7cc10fbf254813a23ea357de0e92a75
zenos631 <nikolay.valkov.uk@gmail.com>
2024-10-21 18:51:03 +0100

    added scrolling functionality

--- src/simple-map.js
+++ src/simple-map.js
@@ -5,7 +5,7 @@ const center = [52.07221, -1.01463];
 
 const MapWrapper = () => {
   return (
-    <MapContainer center={center} zoom={15} scrollWheelZoom={false}>
+    <MapContainer center={center} zoom={15} scrollWheelZoom={true}>
       <TileLayer {...tileLayer} />
     </MapContainer>
   )
