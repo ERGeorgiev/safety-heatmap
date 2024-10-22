35961dfb4b04e56081f9aa3effab1cd63ad35d52
zenos631 <nikolay.valkov.uk@gmail.com>
2024-10-22 19:56:09 +0100

    made custom marker red

--- src/main-map.js
+++ src/main-map.js
@@ -36,6 +36,20 @@ const removeMarker = (index, map, legend) => {
   });
 }
 
+function createMarker(coordinates, map) {
+  const newMarker = L.marker(coordinates, {
+    icon: L.divIcon({
+      className: "custom-icon-marker",
+      iconSize: L.point(40, 40),
+      html: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" class="marker"><path fill-opacity="0.25" d="M16 32s1.427-9.585 3.761-12.025c4.595-4.805 8.685-.99 8.685-.99s4.044 3.964-.526 8.743C25.514 30.245 16 32 16 32z"/><path stroke="#000000" fill="#D7282F" d="M15.938 32S6 17.938 6 11.938C6 .125 15.938 0 15.938 0S26 .125 26 11.875C26 18.062 15.938 32 15.938 32zM16 6a4 4 0 100 8 4 4 0 000-8z"/></svg>`,
+      iconAnchor: [12, 24],
+      popupAnchor: [9, -26],
+    }),
+  })
+  newMarker.bindPopup(`<b>Report unsafe</b>:<br>`);
+  newMarker.addTo(map);
+}
+
 const ShowMarkers = ({ mapContainer, legend, markers }) => {
   return markers.map((marker, index) => {
     return <Marker
@@ -79,8 +93,8 @@ const MyMarkers = ({ map }) => {
       console.log("Click registered, " + marker.length + ' markers on map.');
       removeAllMarkers(map);
       const { lat, lng } = e.latlng;
-      setMarker(mar => [...mar, [lat, lng]]);
-
+      //setMarker(mar => [...mar, [lat, lng]]);
+      createMarker([lat, lng], map);
       info.textContent = `new marker: ${e.latlng}`;
       setLegend(info);
     })
