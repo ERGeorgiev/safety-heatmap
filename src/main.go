c0ea19a1167f6957f5a65e81c01fb7a255b7ab2c
Eduard <e.r.georgiev@gmail.com>
2024-11-01 20:40:07 +0000

    Implemented safetyheatmap backend demo

--- src/main.go
+++ src/main.go
@@ -1,78 +1,121 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 )
 
-type Todo struct {
-	ID        int    `json:"id"`
-	Completed bool   `json:"completed"`
-	Body      string `json:"body"`
+type Point struct {
+	Lat float32 `json:"lat" validate:"required,numeric"`
+	Lon float32 `json:"lon" validate:"required,numeric"`
 }
+
+type HeatPoint struct {
+	Point
+	Intensity float32 `json:"intensity" validate:"required"`
+}
+
+type GetHeatmapRequest struct {
+	Size  string                 `json:"size" validate:"required,min=1,max=1"`
+	Range GetHeatmapRequestRange `json:"range" validate:"required"`
+}
+
+type GetHeatmapRequestRange struct {
+	TopLeft     Point `json:"topleft" validate:"required"`
+	BottomRight Point `json:"bottomright" validate:"required"`
+}
+
+type Heatmap struct {
+	Points []HeatPoint `json:"points"`
+}
+
+var reports []Point = []Point{}
 
 func main() {
 	app := fiber.New()
 
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if os.Getenv("ENV") != "prod" {
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
 	}
 
 	PORT := os.Getenv("PORT")
 
-	todos := []Todo{}
+	app.Post("/api/safetyheatmap/heatmap/get", safetyHeatmapGetHeatmap)
+	app.Post("/api/safetyheatmap/report/add", safetyHeatmapAddReport)
 
-	app.Get("/api/todos", func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(todos)
-	})
-
-	app.Post("/api/todos", func(c *fiber.Ctx) error {
-		todo := &Todo{}
-
-		if err := c.BodyParser(todo); err != nil {
-			return err
-		}
-
-		if todo.Body == "" {
-			return c.Status(400).JSON(fiber.Map{"error": "Todo body is required"})
-		}
-
-		todo.ID = len(todos) + 1
-		todos = append(todos, *todo)
-
-		return c.Status(201).JSON(todo)
-	})
-
-	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error {
-		id := c.Params("id")
-
-		for i, todo := range todos {
-			if fmt.Sprint(todo.ID) == id {
-				todos[i].Completed = true
-				return c.Status(200).JSON(todos[i])
-			}
-		}
-
-		return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
-	})
-
-	app.Delete("/api/todos/:id", func(c *fiber.Ctx) error {
-		id := c.Params("id")
-
-		for i, todo := range todos {
-			if fmt.Sprint(todo.ID) == id {
-				todos = append(todos[:i], todos[i+1:]...)
-				return c.Status(200).JSON(fiber.Map{"success": true})
-			}
-		}
-
-		return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
-	})
+	if os.Getenv("ENV") == "prod" {
+		app.Static("/", ".client/dist")
+	}
 
 	log.Fatal(app.Listen(":" + PORT))
 }
+
+func safetyHeatmapAddReport(c *fiber.Ctx) error {
+	request := &Point{}
+
+	if err := c.BodyParser(request); err != nil {
+		return err
+	}
+
+	validate := validator.New()
+	err := validate.Struct(request)
+	if err != nil {
+		var errs validator.ValidationErrors
+		errors.As(err, &errs)
+		for _, validationError := range errs {
+			return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+				"error": map[string]string{
+					"field":   validationError.Field(),
+					"message": validationError.Error(),
+				},
+			})
+		}
+	}
+
+	reports = append(reports, *request)
+
+	return c.SendStatus(http.StatusCreated)
+}
+
+func safetyHeatmapGetHeatmap(c *fiber.Ctx) error {
+	request := &GetHeatmapRequest{}
+
+	if err := c.BodyParser(request); err != nil {
+		return err
+	}
+
+	validate := validator.New()
+	err := validate.Struct(request)
+	if err != nil {
+		var errs validator.ValidationErrors
+		errors.As(err, &errs)
+		for _, validationError := range errs {
+			return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+				"error": map[string]string{
+					"field":   validationError.Field(),
+					"message": validationError.Error(),
+				},
+			})
+		}
+	}
+
+	return c.Status(200).JSON(reports)
+}
+
+// For Loop Example
+// "todo" is a type with an integer ID and todos is an array
+// for i, todo := range todos {
+// 	if fmt.Sprint(todo.ID) == id {
+// 		todos = append(todos[:i], todos[i+1:]...)
+// 		return c.Status(200).JSON(fiber.Map{"success": true})
+// 	}
+// }
