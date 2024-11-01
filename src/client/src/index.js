3210da3a66e3d9f456cb7f0bf517757da44f70e2
Eduard <e.r.georgiev@gmail.com>
2024-11-01 21:25:39 +0000

    Added QueryClient to index.js

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
+const queryClient = new QueryClient();
+
+// cont. https://www.youtube.com/watch?v=lNd7XlXwlho 01:30:00
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
