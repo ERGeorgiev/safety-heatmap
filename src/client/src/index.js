36444773622e6995eba1c81010ad01e6251fc409
Eduard Georgiev <e.r.georgiev@gmail.com>
2024-11-04 00:41:46 +0200

    Manual merge with main

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 import App from "./App";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 
 // cont. https://www.youtube.com/watch?v=lNd7XlXwlho 01:30:00
 ReactDOM.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+    {/* <QueryClientProvider client={queryClient}> */}
       <App />
-    </QueryClientProvider>
+    {/* </QueryClientProvider> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
